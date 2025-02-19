f9d2e54b10430fcf1e85a53081426216941d9fcb
Abhay kumar <siemonab@gmail.com>
2025-02-19 10:40:26 +0530

    updated in employee navigation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HashRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store/index';
@@ -10,14 +11,14 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <Provider store={store}>
       <BrowserRouter>
         <App />
         <ToastContainer />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
